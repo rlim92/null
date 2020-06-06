1ab414c0b23ffe760581a56e14bf5d2abe9f1637
Rich Lim <richardlim92@gmail.com>
2020-06-05 23:57:51 -0400

    Clean up typo

--- frontend/utils/message_utils.js
+++ frontend/utils/message_utils.js
@@ -1,9 +1,4 @@
-// export const fetchMessage = messageId => {
-//     return $.ajax({
-//         method: "GET",
-//         url: `/api/messages/${messageId}`
-//     });
-// };
+
 
 export const fetchDmMsgs = dmId => {
     return $.ajax({
