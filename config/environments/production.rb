6b68d2273c65daf1273dca901f6828f8f401e962
Rich Lim <richardlim92@gmail.com>
2020-06-09 00:03:45 -0400

    Modify for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,11 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  config.action_cable.allowed_request_origins = ["https://null-discord.herokuapp.com", "http://null-discord.herokuapp.com"]
+  # config.action_cable.mount_path = '/cable'
+  # config.action_cable.allow_same_origin_as_host = true
+  config.action_cable.url = "wss://null-discord.herokuapp.com/cable"
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
